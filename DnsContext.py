410fd3c67b49ad942b6abbccd792d4eeaae0a23c
Tobby_000 <q1142741687@163.com>
2026-01-28 23:11:59 +0800

    feat:重写了domain解析逻辑

--- DnsContext.py
+++ DnsContext.py
@@ -1,23 +1,24 @@
 from Domain import *
 from TypeAdapter import *
 class DnsQuestion:
-    domain:str
+    domain:Domain
     dns_type:int
     dns_class:int
-    def __init__(self,domain:str="",dns_type:int=1,dns_class:int=1):
+    def __init__(self,domain:Domain=Domain(),dns_type:int=1,dns_class:int=1):
         self.domain=domain
         self.dns_type=dns_type
         self.dns_class=dns_class
     @classmethod
     def from_bytearray(cls,index:int,byte:bytearray):
         inst=cls()
-        inst.domain,index=domain_decoder(index,byte)
+        inst.domain,index=inst.domain.domain_decoder(index,byte)
         inst.dns_type,index=read_bytes(index,byte,2,True)
         inst.dns_class,index=read_bytes(index,byte,2,True)
         return inst,index
     def to_bytearray(self):
         byte=bytearray()
-        byte.extend(domain_encoder(self.domain))
+        self.domain.encoder()
+        byte.extend(self.domain.hex)
         byte.extend(self.dns_type.to_bytes(2,'big'))
         byte.extend(self.dns_class.to_bytes(2,'big'))
         return byte
@@ -25,10 +26,9 @@ class DnsQuestion:
         print("Domain",self.domain)
         print("Type",self.dns_type)
         print("Class",self.dns_class)
-class DnsMessage:
-    domain:str
-    dns_type:str
-    dns_class:str
-
+class DnsQuery:
+    domain:Domain
+    dns_type:int
+    dns_class:int
     def __int__(self):
         return
--- Domain.py
+++ Domain.py
@@ -1,40 +1,56 @@
-def domain_encoder(domain:str)->bytearray:
-    domain_sp=domain.split(".")
-    domain_hex=bytearray()
-    for sp in domain_sp:
-        domain_hex.append(len(sp))
-        domain_hex+=sp.encode('ascii')
-    domain_hex.append(0)
-    return domain_hex
-def domain_decoder(index:int,domain_hex,depth=0):
-    #index索引，domain_hex原始二进制，depth深度   输出domain解析后的域名，after_domain域名后的索引
-    domain = ""
-    after_domain=index
-    i=0
-    while i<10:
-        if domain_hex[after_domain]&0xC0==0xC0:
-            if after_domain!=index:
-                domain+="."
-            pos = int.from_bytes(domain_hex[after_domain:after_domain+2])-0XC000
-            if domain_hex == "":
-                print("domain_decode ERROR:full_domain_hex is empty")
-            if depth > 10:
-                print("domain_decode ERROR:depth is too high")
-            domain_full, pos = domain_decoder(pos, domain_hex, depth + 1)
-            domain += domain_full
-            after_domain += 2
-            break
-        else:
-            domain_num=domain_hex[after_domain]
-            if domain_num==0:
-                after_domain=after_domain+1
-                if after_domain==index:
-                    domain+="."
-                break
-            if after_domain!=index:
-                domain+="."
-            domain_sp=domain_hex[after_domain+1:after_domain+domain_num+1].decode('ascii')
-            domain+=domain_sp
-            after_domain+=1+domain_num
+class Domain:
+    string:str
+    hex:bytearray
+    after:int
+    def __init__(self,domain_str:str="",domain_hex:bytearray=None,after_domain:int=None):
+        self.string=domain_str
+        self.hex=domain_hex
+        self.after=after_domain
+        return
+    def encoder(self)->bytearray:
+        domain_sp=self.string.split(".")
+        domain_hex=bytearray()
+        for sp in domain_sp:
+            domain_hex.append(len(sp))
+            domain_hex+=sp.encode('ascii')
+        domain_hex.append(0)
+        self.hex=domain_hex
+        return domain_hex
+    @classmethod
+    def decoder(cls,index:int,byte:bytearray):# index 域名起始索引，byte完整二进制
+        pos:int=index #当前解析指针
+        after_pos:int=index #解析结束后指针
+        inst=cls() # 返回对象
+        i=0 #迭代上限，包括域名字符解析和指针
+        is_po=False #如果在index都出现压缩指针，则将解析后位置挪动到指针后
+        pos_array=set()
+        pos_array.add(index)
+        while i<10:
+            if byte[pos]&0XC0==0XC0:#压缩指针处理
+                if not is_po:
+                    after_pos=pos+2#结束指针挪动到压缩指针后
+                    is_po=True
+                pos=int.from_bytes(byte[pos:pos+2], 'big')&0X3fff
+                if pos in pos_array:
+                    print("ERROR:指针循环")#循环判定
+                    raise Exception("指针循环")
+                pos_array.add(pos)
+            else:
+                char_num=byte[pos]
+                pos+=1
+                if char_num==0:# 如果为0则到达域名末端
+                    if pos==index+1:#处理根域
+                        inst.string+="."
+                    if is_po:
+                        inst.after=after_pos
+                    else:
+                        inst.after=pos
+                    return inst
+                try:
+                    char=byte[pos:pos+char_num].decode('ascii')#主解码
+                except UnicodeDecodeError:
+                    raise ValueError("解码错误，未知字符")
+                inst.string+=char+"."#域名分割
+                pos=pos+char_num
             i+=1
-    return domain,after_domain
+        raise Exception("超过循环上限")
